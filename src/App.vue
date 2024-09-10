075bd3c66dbd1c377a13e59fd09e6b24d6718b6e
KhushiA <khushi.agarwal@argusoft.in>
2024-09-10 14:47:01 +0530

    conditional

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
-  <!-- Hello Vishwas -->
-  <!-- text interpolation -->
-  <!-- moustache syntax -->
   <div>{{ greet }} {{ name }}</div>
   <div v-text="channel"></div>
   <div v-html="channel"></div>
   <h2 v-bind:id="headingId">hwllo</h2>
   <button v-bind:disabled="isDisabled">bind</button>
-  <!-- satic class -->
-  <!-- <h2 class="underline" v-bind:class="status">d</h2> -->
+
   <h2 v-bind:class="underline">d</h2>
+  <!-- isPromoted is true → Since it's true, the second operand 'promoted' is returned as the result of the expression. -->
   <h2 v-bind:class="'promoted' && isPromoted">Promoted movie</h2>
   <!-- <div v-text='name'>Hello</div> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h2></h2>
 </template>
 
 <script>
