2e757a77f8959b900afd66c548d4278819a0a8bf
KhushiA <khushi.agarwal@argusoft.in>
2024-09-10 14:26:53 +0530

    initAL COMMIT

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- Hello Vishwas -->
+  <!-- text interpolation -->
+  <!-- moustache syntax -->
+  <div>{{ greet }} {{ name }}</div>
+  <div v-text="channel"></div>
+  <div v-html="channel"></div>
+  <h2 v-bind:id="headingId">hwllo</h2>
+  <button v-bind:disabled="isDisabled">bind</button>
+  <!-- satic class -->
+  <!-- <h2 class="underline" v-bind:class="status">d</h2> -->
+  <h2 v-bind:class="underline">d</h2>
+  <h2 v-bind:class="'promoted' && isPromoted">Promoted movie</h2>
+  <!-- <div v-text='name'>Hello</div> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  // components: {
+  //   HelloWorld
+  // }
+  data() {
+    return {
+      greet: "Hello",
+      name: "Vishwas",
+      channel: "<b>code evolution<b>",
+      headingid: "heading",
+      isDisabled: false,
+      status: "danger",
+      isPromoted: true,
+      underline: "underline",
+      // isSoldOut
+    };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +47,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.underline {
+  text-decoration: underline;
+}
+.promoted {
+  font-style: italic;
+}
+.new {
+  color: olivedrab;
+}
+.sold-out {
+  color: red;
 }
 </style>
